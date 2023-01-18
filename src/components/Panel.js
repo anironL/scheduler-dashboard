a99f95ef1030ea12e414ae88c0d163637c2692ac
anironL <viasolaris@gmail.com>
2023-01-18 17:56:35 -0500

    Add panel focus state

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -1,26 +1,16 @@
 import React, { Component } from "react";
 
 class Panel extends Component {
-  // The key can use the value from panel.id
-  // The id can use the value from panel.id
-  // The label can use the value from panel.label
-  // The value can use the value from panel.value
-
   render() {
-    const { label, value } = this.props;
+    const { id, label, value, onSelect } = this.props;
 
     return (
-      <section
-        className="dashboard__panel"
-      >
+      <section className="dashboard__panel" onClick={() => onSelect(id)}>
         <h1 className="dashboard__panel-header">{label}</h1>
         <p className="dashboard__panel-value">{value}</p>
-        <p>hi</p>
       </section>
     );
   }
 }
 
-export default { Panel }; 
-
-
+export default Panel;
